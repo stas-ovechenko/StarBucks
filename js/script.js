eb781cd32e22d5a557b6f5be18a32c5db4e7c5cb
Stas <user975345@gmail.com>
2026-02-06 18:01:49 +0300

    stas 975345

--- js/script.js
+++ js/script.js
@@ -30,6 +30,52 @@ document.getElementById('btn').addEventListener('click', () => {
 });
 
 document.getElementById('close_button').addEventListener('click', () => {
+    validation.refresh();
+    document.getElementById('menu_form').classList.remove('active_menu_form');
+    document.getElementById('body').classList.remove('active_form');
+    document.getElementById('active_form_filter').classList.remove('active_filter');
+});
+
+const form = document.getElementById('dropdown_menu');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
+let validation = new JustValidate('#dropdown_menu');
+
+validation.addField('#name', [
+    {
+        rule: "required",
+        errorMessage: "Enter name!"
+    },
+    {
+        rule: "minLength",
+        value: 2,
+        errorMessage: "Minimum 2 value!"
+    }
+]).addField('#tel', [
+    {
+        rule: "required",
+        errorMessage: "Enter phone!"
+    },
+    {
+        rule: "minLength",
+        value: 10,
+        errorMessage: "Minimum 10 value!"
+    }
+]).addField("#msg", [
+    {
+    rule: "required",
+    errorMessage: "Enter Message!"
+    },
+    {
+    rule: "minLength",
+    value: 10,
+    errorMessage: "Minimum 10 value!"
+    }
+]).onSuccess(() => {
+    form.reset();
     document.getElementById('menu_form').classList.remove('active_menu_form');
     document.getElementById('body').classList.remove('active_form');
     document.getElementById('active_form_filter').classList.remove('active_filter');
